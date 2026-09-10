2e7db4feea1d4cd29bb392d5637214d5403795ca
emersonnogueiradev <emersonnogueira100@hotmail.com>
2026-09-10 15:07:50 -0300

    Ajusta rotas para GitHub Pages

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,28 +1,26 @@
-import {Route, createBrowserRouter, createRoutesFromElements } from "react-router-dom";	
+import {
+    Route,
+    createBrowserRouter,
+    createRoutesFromElements
+} from "react-router-dom";
+
 import Layout from "../components/template/Layout";
 import Inicio from "../components/template/Inicio";
 import Projetos from "../components/template/Projetos";
 import Contatos from "../components/template/Contato";
 
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<Layout />}>
+            <Route index element={<Inicio />} />
+            <Route path="inicio" element={<Inicio />} />
+            <Route path="projetos" element={<Projetos />} />
+            <Route path="contatos" element={<Contatos />} />
+        </Route>
+    ),
+    {
+        basename: import.meta.env.PROD ? "/site_pessoal" : "/"
+    }
+);
 
-
-const router = createBrowserRouter(createRoutesFromElements(
-
-    <Route path="/" element={<Layout/>} >
-        
-        <Route index  element={<Inicio/>}/>
-        <Route path="inicio"  element={<Inicio/>}/>        
-        <Route path="projetos"  element={<Projetos/>}/>        
-
-        <Route path="contatos"  element={<Contatos/>}/>
-    </Route>
-
-
-
-
-
-))
-
-
-
-export default router
+export default router;
--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,12 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/site_pessoal/',
-  plugins: [react(),tailwindcss()],
-  server:{
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
+  server: {
     port: 3000
-  },
-
+  }
 })
